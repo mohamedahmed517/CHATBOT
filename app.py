488d2c497e1825dbeb6f937e5b600dbabf24d455
Mohamed Ahmed <125583635+mohamedahmed517@users.noreply.github.com>
2024-11-06 15:45:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 import subprocess
 
+commands = [
+    'pip install pip3-autoremove',
+    'pip-autoremove torch torchvision torchaudio -y',
+    'pip install torch torchvision torchaudio xformers --index-url https://download.pytorch.org/whl/cu121',
+    'pip install unsloth',
+    'pip install azure-ai-textanalytics'
+]
+
 def install_packages():
     
     for command in commands:
